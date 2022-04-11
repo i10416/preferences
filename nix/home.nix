e061fe4d6061097ad4b4dc474ea060418da3c5d2
i10416 <ito.yo16uh90616@gmail.com>
2022-04-11 14:22:31 +0900

    add nixconf

---
+++ nix/home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "i10416";
+  home.homeDirectory = "/Users/i10416";
+  home.language.base = "en_US.UTF-8";
+  fonts.fontconfig.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+    enableSyntaxHighlighting = true;
+    envExtra = ''
+      if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
+        . ~/.nix-profile/etc/profile.d/nix.sh
+      fi
+    '';
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+  };
+  programs.gh = { enable = true; };
+  programs.git = {
+    enable = true;
+    ignores = [ "*~" "*.swp" ".DS_Store" ];
+    userName = "i10416";
+    userEmail = "ito.yo16uh90616@gmail.com";
+  };
+  home.packages = [ pkgs.git pkgs.nixfmt pkgs.jdk11 pkgs.sbt pkgs.openssh ];
+}
