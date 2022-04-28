568b09123843e8d48bd9dae79e132d4364297ec3
110416 <ito.yo16uh90616@gmail.com>
2022-04-28 15:24:57 +0900

    Update home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -68,13 +68,26 @@
       number = true;
     };
     extraConfig = ''
+      set encoding=utf8
+
       set autoindent  " better indent
       set smartindent " even better indent
       set tabstop=4
+
+      set cursorline " highlight cursor line
+      set list " show invisible chars
       set wrap " wrap lines
-      set whichwrap=b,s,h,l,<,>,[,] 
-      set encoding=utf8 
+      set whichwrap=b,s,h,l,<,>,[,] " move to pre/next line when cursor is at the end of line
+
+      set scrolloff=8
+
       set showmatch  " highlight matching parentheses/ brackets
+      set hlsearch " highlight search text
+      set incsearch " search incrementally
+      set wrapscan " back to first match item after the last one
+
+      set clipboard=unnamed,unnamedplus " sync clipboard with OS
+
       set visualbell " blink cursor on error, instead of beeping
 
     '';
