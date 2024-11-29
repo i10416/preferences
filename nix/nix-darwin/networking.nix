2e95adff716bfebb573af08b19504b5eb7218dbc
i10416 <ito.yo16uh90616@gmail.com>
2024-11-29 18:50:19 +0900

    tweak networking

--- nix/nix-darwin/networking.nix
+++ nix/nix-darwin/networking.nix
@@ -3,12 +3,20 @@
   networking = {
     dns = [
       "8.8.8.8"
+      "8.8.4.4"
+      "1.1.1.1"
+      "1.0.0.1"
+      "2001:4860:4860::8888"
+      "2001:4860:4860::8844"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
       "127.0.0.1"
     ];
     search = [ "local" ];
     knownNetworkServices = [
-      "Ethernet"
       "Wi-Fi"
+      "Ethernet Adaptor"
+      "Thunderbolt Ethernet"
     ];
   };
 }
